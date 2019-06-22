561763b6dc66b88b9186e0eb69f9dc7a47aa2e33
lschmidt <leandro.schmidt90@gmail.com>
2019-06-22 18:43:03 -0300

    Added get and post

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ const cors = require('cors')
 const app = express()
 const PORT = 4000
 
-const db = [
-  { id: 'qwhfuaos', name: 'Lucas', lastName: 'Prieto', detail: 'hola que tl sarasa' },
-  { id: 'qwhfuaos', name: 'Leandro', lastName: 'sarasdasd', detail: 'hola que tl sarasa' },
-  { id: 'qwhfuaos', name: 'Drope', lastName: 'sarasa2', detail: 'hola que tl sarasa' },
-  { id: 'qwhfuaos', name: 'Miru', lastName: 'sarasa', detail: 'hola que tl sarasa' },
-]
+const db = { asd: { asd: [] } }
 
-const agregar = item => db.push(item)
-const buscar = () => db
+const agregar = (key, name, item) => {
+  if(!db[key]) {
+    db[key] = {}
+  }
+  if(!db[key][name]) {
+    db[key][name] = []
+  }
+  db[key][name].push(item)
+}
+
+const buscar = (key, name) => {
+  if (db[key] && db[key][name]) {
+    return db[key][name]
+  } else {
+    return []
+  }
+}
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -23,20 +33,24 @@ app.get('/hola/:name', (req, res) => {
   res.status(200).send(`Hola ${name}`)
 })
 
-app.get('/submitions/:id', (req, res) => {
+app.get('/submitions/:name', (req, res) => {
+  const { name } = req.params
+  const { authorization: key } = req.headers
   // ir a buscar a la base de datos
   // form has: name, lastname, email, phone, detail
-  const result = buscar()
+  const result = buscar(key, name)
 
   //devolver
   res.status(200).send(result)
 })
 
-app.post('/submitions', (req, res) => {
+app.post('/submitions/:name', (req, res) => {
+  const { name } = req.params
+  const { authorization: key } = req.headers
   // recibir datos del body del request
   const item = req.body
   // guardar los datos en la base de datos
-  agregar(item)
+  agregar(key, name, item)
   // Enviar un OK
   res.status(200).send()
 })
